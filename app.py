1ea60384767e94c2c5d1862c38a337b39470d0a4
KateBOTTERILL <Kate.BOTTERILL@shaftesburyschool.co.uk>
2025-10-13 09:11:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,11 +27,12 @@ import streamlit as st
 
 warning_message = """
 - **Instructions for students:**
+    - Staff may be using a room for lesson preparation
+    - If the teacher requests the room or it is used for a cover class, please respect this and find somewhere else to work  
     - Strictly no eating  
     - Quiet study only  
     - Rooms left as you found them (tidy!)  
     - Behaviour must be impeccable at all times  
-    - If the teacher requests the room or it is used for a cover class, please respect this and find somewhere else to work  
     - If students misuse a school space, they are liable to be sanctioned in accordance with the school behaviour policy  
     - Where ongoing concerns occur, the room will be removed from the free rooms list  
 """
