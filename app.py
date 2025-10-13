352b64d59d2f64698062d03dbea07150dd8dd3cd
KateBOTTERILL <Kate.BOTTERILL@shaftesburyschool.co.uk>
2025-10-13 08:29:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ df = pd.read_excel("Free rooms Oct25.xlsx", engine="openpyxl")
 period_columns = df.columns[1:]
 
 # Streamlit UI
-st.title("Shaftesbury School Free Room Checker")
+st.title("Free Room Checker")
 selected_periods = st.multiselect("Select one or more periods:", period_columns)
 
 if selected_periods:
@@ -21,3 +21,12 @@ if selected_periods:
     st.write(filtered_df[["Room"] + selected_periods])
 else:
     st.info("Please select at least one period to see available rooms.")
+    st.info("Instructions for students:
+Strictly no eating 
+Quiet study only 
+Rooms left as you found them (tidy!) 
+Behaviour must be impeccable at all times 
+If the teacher requests the room or it is used for a cover class then please respect this and find somewhere else to work 
+If students misuse a school space, they are liable to be sanctioned in accordance with the school behaviour policy 
+Where ongoing concerns occur, the room will be removed from the free rooms list 
+")
